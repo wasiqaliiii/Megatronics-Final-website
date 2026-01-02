ae0a2441a4b7306c224263ac323f0c0265ce8cee
wasiqaliiii <wasiq.ali.189@gmail.com>
2026-01-02 22:31:25 +0500

    3rd commit

--- src/Global/MainComponent.js
+++ src/Global/MainComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import MHome from '../men/pages/MHome';
-import WHome from '../women/pages/WHome';
 import Services from '../Services/Pages/Services';
 import ContactUs from '../Contact Us/Pages/ContactUs';
 import AboutUs from '../About us/pages/AboutUs';
@@ -12,7 +11,6 @@ const MainComponent = () => {
   return (
     <>
       {collection === 'men' && <MHome />}
-      {collection === 'women' && <WHome />}
       {collection === 'services' && <Services />}
       {collection === 'contactUS' && <ContactUs />}
       {collection === 'aboutUs' && <AboutUs />}
